0865b20e8af24affd21fa00672456ff2d15f8654
reyhanwiyasa <reyhanwiyasa@gmail.com>
2024-04-16 20:24:32 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/bookdetail/BookDetailApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bookdetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDetailApplication.class, args);
+    }
+
+}
